2e600a1a0d9ecd76f3bd1860eb197a47695dbd97
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-03-08 20:18:40 +1000

    items api

---
+++ src/api/items/get-item.js
@@ -0,0 +1,28 @@
+const { error } = require("node:console");
+
+async function getItem(itemId, token) {
+    const url = `${import.meta.env.VITE_API_URL}/items/${itemId}/`;
+
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+    });
+
+    if (!response.ok) {
+        const fallBackError = `Error fetching item with id ${itemId}`;
+
+        const data = await response.json().catch(() => {
+            throw new Error(fallBackError);
+        });
+
+        const errorMessage = data?.detail ?? fallBackError;
+        throw new Error(errorMessage);
+    }
+
+    return await response.json();
+
+}
+
+export default getItem;
